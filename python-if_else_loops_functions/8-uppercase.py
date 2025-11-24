afac08fc8d3d347abe3345f61e99f7c55a759b75
AyhanAghayev <agayevayhan5@gmail.com>
2025-11-24 12:35:54 +0000

    default commit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        print("{}".format(chr(ord(i)+32)), end="")
-
+        if i.lower() == i:
+            print("{}".format(chr(ord(i)-32)), end="")
