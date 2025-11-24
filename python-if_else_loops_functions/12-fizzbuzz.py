2ea6f2753d0fc9e39ed11c7e856b136f976d03f1
AyhanAghayev <agayevayhan5@gmail.com>
2025-11-24 14:45:41 +0000

    default commit

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(1, 100):
+for i in range(1, 101):
     if i % 5 == 0 and i % 3 == 0:
         print("FizzBuzz", end=" ")
     elif i % 3 == 0:
@@ -8,4 +8,3 @@ for i in range(1, 100):
         print("Buzz", end=" ")
     else:
         print(i, end=" ")
-print("Buzz")
