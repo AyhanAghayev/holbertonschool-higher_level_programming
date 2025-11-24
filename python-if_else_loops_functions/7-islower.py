a4bf417745180d45d7a0d7d2a09d6c7c27d1faad
AyhanAghayev <agayevayhan5@gmail.com>
2025-11-24 12:14:45 +0000

    default commit

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-#def islower(c):
-#    if c=='':
-#        print()
-#    if c.islower():
-#        return True
-#    else:
-#        return False
 
 def islower(c):
-    # Check that c is a single character string
     if not isinstance(c, str) or len(c) != 1:
         raise TypeError("c must be a single character string")
 
-    # Use Python's built-in islower()
     return c.islower()
 
 
 islower = __import__('7-islower').islower
-
-
