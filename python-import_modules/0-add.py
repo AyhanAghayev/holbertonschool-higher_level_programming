c721dfa8726bf40a0ccdc72d196a24a6ee2cc888
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-01 16:21:10 +0400

    def commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,4 +4,4 @@ from add_0.py import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{a} + {b} = {c}".format(a=a, b=b, c=add(a, b)))
+    print("{a} + {b} = {c:d}".format(a=a, b=b, c=add(a, b)))
