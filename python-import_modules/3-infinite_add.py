f4ba9ab9f9589e5a67c8e2776b972766e22a59b3
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-03 19:52:19 +0400

    def commit

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
 
     if len(args) - 1 == 0:
         print(0)
-    for i in range(1, len(args)):
-        sum = sum + int(args[i])
+    else:
+        for i in range(1, len(args)):
+            sum = sum + int(args[i])
     print(sum)
