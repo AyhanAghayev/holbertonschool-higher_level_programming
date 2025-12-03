c8b9f7c09a09f0391991f1efdf46f03bccf77d2c
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-03 19:41:35 +0400

    Second task

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    args = len(sys.argv) - 1
+
+    if args == 0:
+        print("0 arguments.")
+    elif args == 1:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print(f"{args} arguments:")
+        for i in range(1, args + 1):
+            print(f"{i}: {sys.argv[i]}")
+            
