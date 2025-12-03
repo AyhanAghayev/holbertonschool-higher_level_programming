a53f8b192514d103114303f63a32b63f4691309f
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-03 19:51:07 +0400

    def commit

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv
+
+    sum = 0
+
+    if len(args) - 1 == 0:
+        print(0)
+    for i in range(1, len(args)):
+        sum = sum + args[i]
+    print(sum)
