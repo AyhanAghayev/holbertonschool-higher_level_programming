6412e1c399bdcd56f37543fcf0c51b1584e7f120
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-04 15:47:32 +0400

    def commit

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence) if len(sentence) > 0 else None
-    return (length, sentence[0])
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else None
+    return (length, first_char)
