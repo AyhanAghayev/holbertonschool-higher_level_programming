fee43ccf2c4fcfab309a9e771c06c81b983974e3
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-04 15:23:26 +0400

    def commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print(" ".join("{:d}".format(j)), end="")
+            print(" ".join("{:d}".format(j) for j in i), end="")
         print()
