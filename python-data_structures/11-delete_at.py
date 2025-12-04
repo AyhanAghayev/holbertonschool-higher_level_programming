9e7bb85e05289c1f55c038e9aa4838d17dc4d2a2
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-04 15:58:11 +0400

    def commit

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,5 +4,5 @@ def delete_at(my_list=[], idx=0):
     for i, val in enumerate(my_list):
         if i != idx:
             new_list.append(val)
-    my_list = new_list
+    my_list[:] = new_list
     return new_list
