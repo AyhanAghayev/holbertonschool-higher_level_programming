c427aa38f8dfdd536c845e69dfb6502eaf14271a
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-08 19:14:24 +0400

    zaaa

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     roman_keys = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500}
     sum = 0
     for i in roman_string:
-        if sum <= roman_keys[i]:
+        if sum >= roman_keys[i]:
             sum += roman_keys[i]
         else:
             sum = roman_keys[i] - sum
