6aac33649eec04bceca4f5ac15c0aead4ee0876f
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-08 18:38:45 +0400

    zaaa

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     value = a_dictionary.get(key, None)
-    if value != None:
+    if value is not None:
         a_dictionary.pop(key)
     return a_dictionary
