12b9208687663bf9bebb212c9fd30b33a555e71c
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-08 17:20:48 +0400

    zaaa

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
 
     def square_matrix_simple(matrix=[]):
-        newtrix = [][]
+        newtrix = [3][3]
         for rows in matrix:
             for i in rows:
                 newtrix[rows].append(i*i)
