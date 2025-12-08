cff24aec079c7a1ab61885616e6e5a643cb55d28
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-08 18:45:31 +0400

    zaaa

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    dicti = a_dictionary.copy
+    dicti = a_dictionary.copy()
     for i in dicti.keys():
         dicti.update({i: dicti[i] * 2})
     return dicti
