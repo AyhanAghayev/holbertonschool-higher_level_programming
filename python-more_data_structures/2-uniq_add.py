6ded64edc9cf424ca6123948a00e49e87cdbedfc
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-08 18:06:41 +0400

    zaaa

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,9 +2,9 @@
 def uniq_add(my_list=[]):
     new_list = []
     sum = 0
-    for i in my_list:
-        if my_list.count(i) < 2:
-            new_list.append(i)
+    for index, value in enumerate(my_list):
+        if len(new_list) != 0 and new_list[index] != value:
+            new_list.append(value)
     for i in new_list:
         sum += i
     return sum
