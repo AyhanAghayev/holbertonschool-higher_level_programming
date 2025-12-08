2b06330f6ab2e32c7d54643af3e5e355eef1e72a
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-08 18:23:32 +0400

    zaaa

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print(a_dictionary.keys.sort())
+    sorted_keys = sorted(a_dictionary.keys())
+    for i in sorted_keys:
+        print("{}: {:d}".format(sorted_keys, a_dictionary[sorted_keys]))
