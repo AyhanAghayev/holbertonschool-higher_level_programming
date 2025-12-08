d3f7579662997eb1b22a818d6d7affbb9d366fee
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-08 19:21:13 +0400

    zaaa

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     roman_keys = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500}
     sum = 0
     for index, value in enumerate(roman_string):
+        if index == 0:
+            sum += roman_keys[value]
         if roman_keys[roman_string[index-1]] >= roman_keys[value]:
             sum += roman_keys[value]
         else:
