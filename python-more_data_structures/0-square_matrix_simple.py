db2e36e24aa3db8ae117163f9f03898f9c8ae51e
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-08 17:09:36 +0400

    slm

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    def square_matrix_simple(matrix=[]):
+        newtrix = [][]
+        for rows in matrix:
+            for i in rows:
+                newtrix[rows].append(i*i)
+        return newtrix
