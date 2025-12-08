af1cb19f6b267b52833e9c38645ca1ce56016e65
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-08 18:51:51 +0400

    zaaa

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    keys = a_dictionary.keys()
-    max_score = max(keys)
+    max_score = max(a_dictionary, key = a_dictionary.get)
     return max_score
