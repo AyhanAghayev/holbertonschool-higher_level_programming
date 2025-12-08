17aff13889a83301a4e5e8f0b57c2da3c60accb1
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-08 19:33:24 +0400

    zaaa

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    elif len(roman_string) == 0:
+        return None
     roman_keys = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500}
     sum = 0
     for index, value in enumerate(roman_string):
