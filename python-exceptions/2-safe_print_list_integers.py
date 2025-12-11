35455f406f868c56187fc38802e5144a3cac0adc
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-11 13:54:20 +0400

    zaa????

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         print()
         return int(count)
-    except:
-        print()
-        return int(count)
+    except Exception as e:
+        print(e)
