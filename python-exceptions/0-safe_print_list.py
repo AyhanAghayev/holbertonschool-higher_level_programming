9f08063168b4ab70239389e9e19ae970f7fb15dd
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-11 11:21:46 +0400

    Zaaa

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         print()
         return int(count)
-    except TypeError:
+    except IndexError:
         print()
         return int(count)
