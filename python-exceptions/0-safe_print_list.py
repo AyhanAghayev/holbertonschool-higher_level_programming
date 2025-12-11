2ecf36105303361bff5bb8fb92e96d8193538859
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-11 11:17:53 +0400

    Zaaa

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,10 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            if x > len(my_list):
-                break
             print(my_list[i], end="")
         print()
         return int(x)
     except:
-        print("An error occured")
+        return int(x)
