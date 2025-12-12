c84dc95a7633cd85d6a7e45fba85dfb556b6464b
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-12 12:42:46 +0400

    z

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def square(i):
-    return i * i
-
 def square_matrix_map(matrix=[]):
-    return list(map(lambda x: list(map(square, x)), matrix))
+    return list(map(lambda x: list(map(lambda squared: squared*squared, x)), matrix))
