ee9d842e0270179a233da74c8b46402ff933ba96
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-12 13:31:04 +0400

    z

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -2,7 +2,11 @@
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
-        if i > a:
-            raise Exception("Too far")
-        result = a ** b / i
-
+        try:
+            if i > a:
+                raise Exception("Too far")
+            result += a ** b / i
+        except:
+            result = a + b
+            break
+    return result
