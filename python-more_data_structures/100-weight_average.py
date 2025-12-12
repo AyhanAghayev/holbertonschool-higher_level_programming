b59ccde734cd2587b1ad9bd4d2a2beb4e61d657d
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-12 12:27:23 +0400

    z

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -2,8 +2,12 @@
 def weight_average(my_list=[]):
     result = 0
     weight = 0
-    for i in my_list:
-        result += i[0] * i[1]
-        weight += i[1]
-    result = result / weight
-    return result
+    try:
+        for i in my_list:
+            result += i[0] * i[1]
+            weight += i[1]
+        result = result / weight
+    except ZeroDivisionError:
+        return 0.0
+    finally:
+        return result
