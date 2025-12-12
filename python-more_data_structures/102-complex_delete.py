0de35956f11336295db4ad0a32bbb2a2dfa1d15f
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-12 12:51:42 +0400

    z

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-        for key, val in a_dictionary:
+        for key, val in a_dictionary.items():
             if val == value:
                 a_dictionary.pop(key)
         return a_dictionary
