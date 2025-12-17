b4b0cf095cade43a09ca868b48af6f8c93aa81ae
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-17 17:59:30 +0400

    niga

---
+++ python-classes/5-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module defines a square class with a private attribute size"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        print("#{}".format(self.__size ** 2))
