323834a23b03d558b0424f4a333e87fb42296bc5
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-17 18:24:35 +0400

    niga

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Defines a square"""
-    def __init__(self, size, position=(0, 0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
 
