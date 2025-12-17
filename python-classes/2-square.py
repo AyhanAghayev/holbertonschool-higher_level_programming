753e5c76d4448c715d7d6d1db485c5af683cd875
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-17 17:37:57 +0400

    niga

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,10 +5,9 @@
 class Square():
     """class"""
     def __init__(self, size=0):
-        try:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            self.__size = size
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        self.__size = size
         
