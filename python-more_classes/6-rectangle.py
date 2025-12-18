4ed7835387706c9c863217ab9abdee700e4f6a6e
AyhanAghayev <agayevayhan5@gmail.com>
2025-12-18 15:22:12 +0400

    Za

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        self.number_of_instances += 1
+        number_of_instances += 1
 
     @property
     def width(self):
