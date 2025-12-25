5e80af6ec0c7cf3a66af90fe8d1cf26c1990857f
ecma <agayevayhan5@gmail.com>
2025-12-25 16:48:59 +0400

    Holbing my ton

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,7 +4,6 @@
 class MyList(list):
     """MyList class inherits from list"""
 
-    @instancemethod
     def print_sorted(self):
         """Prints the list in ascending order"""
         print(sorted(self))
