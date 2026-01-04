49abb8e1b2111b5282c2568ed45a447257951063
ecma <agayevayhan5@gmail.com>
2026-01-04 16:34:07 +0400

    Holbing my ton

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""suckerton"""
+
+
+def append_write(filename="", text=""):
+    """aisdjoawdj"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
