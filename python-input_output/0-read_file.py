3e89dea538db9b29e54dbac4293108600163a408
ecma <agayevayhan5@gmail.com>
2026-01-04 16:25:52 +0400

    Holbing my ton

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""suckerton"""
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-    # The end="" prevents adding an extra newline after the content
