6fb4f93a8a2f8862e2a514f78c6a2bc5f168f2e9
ecma <agayevayhan5@gmail.com>
2026-01-04 16:26:58 +0400

    Holbing my ton

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 """suckerton"""
 
 def read_file(filename=""):
+    
     """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
