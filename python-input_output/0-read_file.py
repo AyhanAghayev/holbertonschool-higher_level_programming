b16bc8fe616a583ec66e3d0889184d42cd1cbc68
ecma <agayevayhan5@gmail.com>
2026-01-04 16:23:40 +0400

    Holbing my ton

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,3 +3,4 @@ def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
+    # The end="" prevents adding an extra newline after the content
